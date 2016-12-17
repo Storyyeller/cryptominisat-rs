13f1fbe705d756681010a9fdaa7a91ac898e68b8
Robert Grosse <n210241048576@gmail.com>
2016-12-17 11:55:30 -0800

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ extern crate libc;
 use libc::size_t;
 use std::slice;
 
+/// The maximum number of variables allowed by the solver
 pub const MAX_NUM_VARS: size_t = (1 << 28) - 1;
 
 // cryptominisat types
@@ -19,6 +20,7 @@ enum SATSolver {} // opaque pointer
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Lit(u32);
 impl Lit {
+    /// Returns None if var >= 1 << 31, but you should not rely on var >= MAX_NUM_VARS
     pub fn new(var: u32, negated: bool) -> Option<Lit> {
         if var < (1 << 31) {
             Some(Lit(var << 1 | (negated as u32)))
@@ -26,15 +28,18 @@ impl Lit {
             None
         }
     }
+    /// The underlying variable
     pub fn var(&self) -> u32 {
         self.0 >> 1
     }
+    /// Whether this literal is negated
     pub fn isneg(&self) -> bool {
         self.0 & 1 != 0
     }
 }
 impl std::ops::Not for Lit {
     type Output = Lit;
+    /// Negate this literal
     fn not(self) -> Lit {
         Lit(self.0 ^ 1)
     }
@@ -88,47 +93,59 @@ impl Drop for Solver {
     }
 }
 impl Solver {
-    // wrappers
+    /// Create new solver instance
     pub fn new() -> Solver {
         Solver(unsafe { cmsat_new() })
     }
+    /// Current number of variables. Call new_var() or new_vars() to increase this.
     pub fn nvars(&self) -> u32 {
         unsafe { cmsat_nvars(self.0) }
     }
+    /// Current number of variables
     pub fn add_clause(&mut self, lits: &[Lit]) -> bool {
         unsafe { cmsat_add_clause(self.0, lits.as_ptr(), lits.len()) }
     }
+    /// Add a xor clause, which enforces that the xor of the unnegated variables equals rhs.
+    /// It is generally more convienent to use add_xor_literal_clause() instead.
     pub fn add_xor_clause(&mut self, vars: &[u32], rhs: bool) -> bool {
         unsafe { cmsat_add_xor_clause(self.0, vars.as_ptr(), vars.len(), rhs) }
     }
+    /// Adds n new variabless.
     pub fn new_vars(&mut self, n: size_t) {
         unsafe { cmsat_new_vars(self.0, n) }
     }
+    /// Solve and return Lbool::True if a solution was found.
     pub fn solve(&mut self) -> Lbool {
         unsafe { cmsat_solve(self.0) }
     }
+    /// Solve under the assumption that the passed literals are true and return Lbool::True if a solution was found.
     pub fn solve_with_assumptions(&mut self, assumptions: &[Lit]) -> Lbool {
         unsafe { cmsat_solve_with_assumptions(self.0, assumptions.as_ptr(), assumptions.len()) }
     }
+    /// Returns true/false/unknown status for each variable.
     pub fn get_model(&self) -> &[Lbool] {
         unsafe { to_slice(cmsat_get_model(self.0)) }
     }
+    /// Return conflicts for assumptions that led to unsatisfiability.
     pub fn get_conflict(&self) -> &[Lit] {
         unsafe { to_slice(cmsat_get_conflict(self.0)) }
     }
+    /// Set number of threads used for solving. Must not be called after other methods.
     pub fn set_num_threads(&mut self, n: u32) {
         unsafe { cmsat_set_num_threads(self.0, n) }
     }
 
-    // helper functions defined in terms of the above
+    /// Helper that adds a variable and returns the corresponding literal.
     pub fn new_var(&mut self) -> Lit {
         let n = self.nvars();
         self.new_vars(1);
         Lit::new(n as u32, false).unwrap()
     }
+    /// Wrapper which calls get_model() and returns whether the given literal is true.
     pub fn is_true(&self, lit: Lit) -> bool {
         self.get_model()[lit.var() as usize] == Lbool::from(!lit.isneg())
     }
+    /// Wrapper which converts literals to variables and then calls add_xor_clause()
     pub fn add_xor_literal_clause(&mut self, lits: &[Lit], mut rhs: bool) -> bool {
         let mut vars = Vec::with_capacity(lits.len());
         for lit in lits {
